6f00395e364ebf8a63ff318f6fa23a6ddcf1c4d8
chordou <49122703+chordou@users.noreply.github.com>
2019-11-26 10:54:14 +0800

    Add files via upload

--- catching_pictures.py
+++ catching_pictures.py
@@ -30,29 +30,29 @@ def CatchPICFromVideo(window_name, camera_idx, catch_pic_num, path_name):
     
         grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  #将当前桢图像转换成灰度图像            
         
-        #人脸检测，1.2和2分别为图片缩放比例和需要检测的有效点数
+
         faceRects = classfier.detectMultiScale(grey, scaleFactor = 1.2, minNeighbors = 3, minSize = (32, 32))
-        if len(faceRects) > 0:          #大于0则检测到人脸                                   
-            for faceRect in faceRects:  #单独框出每一张人脸
+        if len(faceRects) > 0:
+            for faceRect in faceRects:
                 x, y, w, h = faceRect                        
                 
-                #将当前帧保存为图片
+
                 img_name = '%s/%d.jpg'%(path_name, num)                
                 image = frame[y - 10: y + h + 10, x - 10: x + w + 10]
                 cv2.imwrite(img_name, image)                                
                                 
                 num += 1                
-                if num > (catch_pic_num):   #如果超过指定最大保存数量退出循环
+                if num > (catch_pic_num):
                     break
                 
-                #画出矩形框
+
                 cv2.rectangle(frame, (x - 10, y - 10), (x + w + 10, y + h + 10), color, 2)
                 
-                #显示当前捕捉到了多少人脸图片了，这样站在那里被拍摄时心里有个数，不用两眼一抹黑傻等着
+
                 font = cv2.FONT_HERSHEY_SIMPLEX
                 cv2.putText(frame,'num:%d' % (num),(x + 30, y + 30), font, 1, (255,0,255),4)                
         
-        #超过指定最大保存数量结束程序
+
         if num > (catch_pic_num): break                
                        
         #显示图像
@@ -63,7 +63,7 @@ def CatchPICFromVideo(window_name, camera_idx, catch_pic_num, path_name):
         if c & 0xFF == ord('q'):
             break        
     
-    #释放摄像头并销毁所有窗口
+
     cap.release()
     cv2.destroyAllWindows() 
     
--- recognizing.py
+++ recognizing.py
@@ -13,13 +13,13 @@ from PIL import Image
 def CatchUsbVideo(window_name, camera_idx):
     cv2.namedWindow(window_name)
     
-    #视频来源，可以来自一段已存好的视频，也可以直接来自USB摄像头
+
     cap = cv2.VideoCapture(0)                
     
-    #告诉OpenCV使用人脸识别分类器
+
     classfier = cv2.CascadeClassifier("C:\\Users\\cqiuac\\Downloads\\opencv\\build\\etc\\haarcascades\\haarcascade_frontalface_alt2.xml")
     
-    #识别出人脸后要画的边框的颜色，RGB格式
+    #RGB
     color = (0, 255, 0)
         
     while cap.isOpened():
@@ -27,17 +27,17 @@ def CatchUsbVideo(window_name, camera_idx):
         if not ok:            
             break  
  
-        #将当前帧转换成灰度图像
+
         grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)                 
         
-        #人脸检测，1.2和2分别为图片缩放比例和需要检测的有效点数
+
         faceRects = classfier.detectMultiScale(grey, scaleFactor = 1.2, minNeighbors = 3, minSize = (32, 32))
-        if len(faceRects) > 0:            #大于0则检测到人脸                                   
-            for faceRect in faceRects:  #单独框出每一张人脸
+        if len(faceRects) > 0:
+            for faceRect in faceRects:
                 x, y, w, h = faceRect        
                 cv2.rectangle(frame, (x - 10, y - 10), (x + w + 10, y + h + 10), color, 2)
                         
-        #显示图像
+
         cv2.imshow(window_name, frame)        
         c = cv2.waitKey(10)
         if c & 0xFF == ord('q'):
@@ -51,5 +51,5 @@ if __name__ == '__main__':
     if len(sys.argv) != 1:
         print("Usage:%s camera_id\r\n" % (sys.argv[0]))
     else:
-        CatchUsbVideo("识别人脸区域", 0)
+        CatchUsbVideo("recognizing face", 0)
         
